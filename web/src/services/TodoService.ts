88e7d305c949f9e75bae3cf02d05d95f8d2a4f1c
Collin Giess <68926012+GiessC@users.noreply.github.com>
2024-11-03 18:15:54 -0500

    feat(web): added create todo api integration

--- web/src/services/TodoService.ts
+++ web/src/services/TodoService.ts
@@ -1,17 +1,27 @@
 import TodoItem from '@/domain/TodoItem';
+import BaseError from '@/errors/BaseError';
+import { apiUrl } from '@/utils/api';
+import { StatusCodes } from 'http-status-codes';
 
 export default class TodoService {
     static async create(label: string): Promise<TodoItem> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(
-                    TodoItem.fromJSON({
-                        id: 1,
-                        label,
-                        completed: false,
-                    }),
-                );
-            }, 1000);
+        const request = {
+            label,
+            isCompleted: false,
+        };
+        const response = await fetch(apiUrl('/todo'), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(request),
         });
+        const jsonResponse = await response.json();
+        if (response.status !== StatusCodes.CREATED) {
+            throw new BaseError(
+                jsonResponse.error ?? 'Failed to create todo item.',
+            );
+        }
+        return jsonResponse;
     }
 }
