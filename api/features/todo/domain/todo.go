f481758ac0049a0ed57035d24c6255fcfecbad2c
Collin Giess <68926012+GiessC@users.noreply.github.com>
2024-11-03 20:51:35 -0500

    perf: moved todo id to sk for efficient queries

--- api/features/todo/domain/todo.go
+++ api/features/todo/domain/todo.go
@@ -21,14 +21,14 @@ type TodoOption func(*Todo)
 
 func WithId(todoId string) TodoOption {
 	return func(todoItem *Todo) {
-		todoItem.Pk = getPk("1", todoItem.IsCompleted, todoId)
+		todoItem.Sk = getSk(todoItem.CreatedAt, todoId)
 		todoItem.TodoId = todoId
 	}
 }
 
 func WithCreatedAt(createdAt string) TodoOption {
 	return func(todoItem *Todo) {
-		todoItem.Sk = getSk(createdAt)
+		todoItem.Sk = getSk(createdAt, todoItem.TodoId)
 		todoItem.CreatedAt = createdAt
 	}
 }
@@ -37,8 +37,8 @@ func NewTodo(label string, isCompleted bool, options ...TodoOption) *Todo {
 	todoId := uuid.NewString()
 	createdAt := util.CurrentIsoString()
 	todoItem := &Todo{
-		Pk:          getPk("1", isCompleted, todoId),
-		Sk:          getSk(createdAt),
+		Pk:          getPk("1", isCompleted),
+		Sk:          getSk(createdAt, todoId),
 		TodoId:      todoId,
 		Label:       label,
 		IsCompleted: isCompleted,
@@ -50,10 +50,10 @@ func NewTodo(label string, isCompleted bool, options ...TodoOption) *Todo {
 	return todoItem
 }
 
-func getPk(userId string, isCompleted bool, todoId string) string {
-	return fmt.Sprintf("USER#%s#TODO#COMPLETED#%t#%s", userId, isCompleted, todoId)
+func getPk(userId string, isCompleted bool) string {
+	return fmt.Sprintf("USER#%s#TODO#COMPLETED#%t", userId, isCompleted)
 }
 
-func getSk(createdAt string) string {
-	return createdAt
+func getSk(createdAt string, todoId string) string {
+	return fmt.Sprintf("%s#TODO#%s", createdAt, todoId)
 }
