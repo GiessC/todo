3581566fa14c571d816e760aa7afa64daa324699
Collin Giess <68926012+GiessC@users.noreply.github.com>
2024-11-05 00:14:43 -0500

    added new gsi to config and todo domain

--- api/features/todo/domain/todo.go
+++ api/features/todo/domain/todo.go
@@ -10,6 +10,8 @@ import (
 type Todo struct {
 	Pk          string `dynamodbav:"pk"`
 	Sk          string `dynamodbav:"sk"`
+	Gsi1pk      string `dynamodbav:"gsi1pk"`
+	Gsi1sk      string `dynamodbav:"gsi1sk"`
 	TodoId      string `dynamodbav:"todoId"`
 	Label       string `dynamodbav:"label"`
 	IsCompleted bool   `dynamodbav:"isCompleted"`
@@ -21,14 +23,14 @@ type TodoOption func(*Todo)
 
 func WithId(todoId string) TodoOption {
 	return func(todoItem *Todo) {
-		todoItem.Sk = getSk(todoItem.CreatedAt, todoId)
+		todoItem.Gsi1sk = GetGsi1Sk(todoItem.CreatedAt, todoId)
 		todoItem.TodoId = todoId
 	}
 }
 
 func WithCreatedAt(createdAt string) TodoOption {
 	return func(todoItem *Todo) {
-		todoItem.Sk = getSk(createdAt, todoItem.TodoId)
+		todoItem.Gsi1sk = GetGsi1Sk(createdAt, todoItem.TodoId)
 		todoItem.CreatedAt = createdAt
 	}
 }
@@ -38,8 +40,10 @@ func NewTodo(label string, isCompleted bool, options ...TodoOption) *Todo {
 	createdAt := util.CurrentIsoString()
 	userId := "1"
 	todoItem := &Todo{
-		Pk:          GetPk(userId, isCompleted),
-		Sk:          getSk(createdAt, todoId),
+		Pk:          GetPk(todoId),
+		Sk:          getSk(),
+		Gsi1pk:      GetGsi1Pk(userId, isCompleted),
+		Gsi1sk:      GetGsi1Sk(createdAt, todoId),
 		TodoId:      todoId,
 		Label:       label,
 		IsCompleted: isCompleted,
@@ -52,10 +56,18 @@ func NewTodo(label string, isCompleted bool, options ...TodoOption) *Todo {
 	return todoItem
 }
 
-func GetPk(userId string, isCompleted bool) string {
+func GetPk(todoId string) string {
+	return fmt.Sprintf("TODO#%s", todoId)
+}
+
+func getSk() string {
+	return "TODO"
+}
+
+func GetGsi1Pk(userId string, isCompleted bool) string {
 	return fmt.Sprintf("USER#%s#TODO#COMPLETED#%t", userId, isCompleted)
 }
 
-func getSk(createdAt string, todoId string) string {
-	return fmt.Sprintf("%s#TODO#%s", createdAt, todoId)
+func GetGsi1Sk(createdAt string, todoId string) string {
+	return fmt.Sprintf("TODO#%s#%s", createdAt, todoId)
 }
