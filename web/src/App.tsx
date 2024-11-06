c910e7b2311bd4dff2eebe527843a3363b87b45d
Collin Giess <68926012+GiessC@users.noreply.github.com>
2024-11-05 22:30:21 -0500

    added app-wide error boundary

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,23 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
 import TodoList from './components/todo/TodoList';
 import Providers from './Providers';
 
 function App() {
     return (
-        <Providers>
-            <div className='h-screen w-screen'>
-                <TodoList />
-            </div>
-        </Providers>
+        <ErrorBoundary
+            fallback={
+                <div>
+                    Something went wrong. Please refresh the page and try again.
+                </div>
+            }
+        >
+            <Providers>
+                <div className='h-screen w-screen'>
+                    <TodoList />
+                </div>
+            </Providers>
+        </ErrorBoundary>
     );
 }
 
