eddd91f90d4591f11d420fbbf72b694bd8665ad9
Collin Giess <68926012+GiessC@users.noreply.github.com>
2024-11-05 21:55:38 -0500

    style: added pointer cursor on todo label hover

--- web/src/components/todo/Todo.tsx
+++ web/src/components/todo/Todo.tsx
@@ -43,7 +43,7 @@ const Todo = ({ className = '', todo, onUpdate, onDelete }: TodoProps) => {
                 />
                 <label
                     htmlFor={todo.todoId}
-                    className={`ml-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 truncate break-words text-balance  ${
+                    className={`ml-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 truncate break-words text-balance hover:cursor-pointer ${
                         todo.isCompleted && 'line-through'
                     }`}
                 >
