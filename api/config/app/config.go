2d7817c4efc765f13ee4a56ba867b0e4817503a6
Collin Giess <68926012+GiessC@users.noreply.github.com>
2024-11-08 20:59:36 -0500

    added jwt secret to api config

--- api/config/app/config.go
+++ api/config/app/config.go
@@ -4,4 +4,5 @@ type Config struct {
 	Region            string `env:"TODO__AWS_REGION"`
 	TodoTableName     string `env:"TODO__DYNAMODB_TODO_TABLE_NAME"`
 	TodoGsi1IndexName string `env:"TODO__DYNAMODB_TODO_GSI1_INDEX_NAME"`
+	JwtSecret         string `env:"TODO__JWT_SECRET"`
 }
